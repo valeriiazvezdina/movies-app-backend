048586ecb23de40161a2ec223c4fbbd12a6f9ccd
valeriia-zvezdina <valeriia.zvezdina@sentinelone.com>
2025-01-31 17:41:19 +0100

    feat: add cors trusted server

--- src/main/java/com/zvezdval/movies_api/movie/MovieController.java
+++ src/main/java/com/zvezdval/movies_api/movie/MovieController.java
@@ -3,14 +3,12 @@ package com.zvezdval.movies_api.movie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/movies")
 @RequiredArgsConstructor
